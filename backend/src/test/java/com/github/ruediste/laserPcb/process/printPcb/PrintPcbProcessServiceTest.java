8360f1eb7543575697cd47888b1e298298f5eaa4
Ruedi Steinmann <ruediste@gmail.com>
2022-03-27 16:30:58 +0200

    add unit tests

--- backend/src/test/java/com/github/ruediste/laserPcb/process/printPcb/PrintPcbProcessServiceTest.java
+++ backend/src/test/java/com/github/ruediste/laserPcb/process/printPcb/PrintPcbProcessServiceTest.java
@@ -24,8 +24,18 @@ public class PrintPcbProcessServiceTest {
 	List<CoordinatePoint> cornerPoints = List.of(CoordinatePoint.of(4, 4), CoordinatePoint.of(5, 4),
 			CoordinatePoint.of(3, 6), CoordinatePoint.of(3, 7));
 
+	/** origin is at (3.52,3.92) */
+	List<CoordinatePoint> cornerPointsNonSquare = List.of(CoordinatePoint.of(4, 4), CoordinatePoint.of(10, 5),
+			CoordinatePoint.of(3, 6), CoordinatePoint.of(2, 10));
+
 	/**
-	 * BL: (10,15) BR: (110,15)
+	 * origin is at (12.16,5.36))
+	 */
+	List<CoordinatePoint> cornerPointsNonSquareRight = List.of(CoordinatePoint.of(4, 4), CoordinatePoint.of(10, 5),
+			CoordinatePoint.of(12, 6), CoordinatePoint.of(11, 10));
+
+	/**
+	 * /** BL: (10,15) BR: (110,15)
 	 */
 	Rectangle2D bounds = new Rectangle2D.Double(10, 15, 100, 200);
 
@@ -50,6 +60,17 @@ public class PrintPcbProcessServiceTest {
 		check(t, 10, 15, 3 + 5, 9);
 		check(t, 20, 15, 13 + 5, 9);
 		check(t, 10, 20, 3 + 5, 14);
+	}
+
+	@Test
+	public void calculateTransformation_bl_bl_nonSquare() {
+
+		AffineTransformation t = service.calculateTransformation(Corner.BL, Corner.BL, cornerPointsNonSquare, bounds,
+				2);
+
+		check(t, 10, 15, 5.164, 6.2216);
+		check(t, 20, 15, 15.028, 7.8656);
+		check(t, 10, 20, 4.342, 11.15355);
 	}
 
 	@Test
@@ -104,6 +125,16 @@ public class PrintPcbProcessServiceTest {
 		check(t, 10, 15, 3 - 5, 9);
 		check(t, 20, 15, 3 - 10 - 5, 9);
 		check(t, 10, 20, 3 - 5, 14);
+	}
+
+	@Test
+	public void calculateTransformation_br_bl_nonSquare() {
+		AffineTransformation t = service.calculateTransformation(Corner.BR, Corner.BL, cornerPointsNonSquareRight,
+				bounds, 5);
+
+		check(t, 10, 15, 6.4060, 9.47);
+		check(t, 20, 15, -3.4579, 7.826);
+		check(t, 10, 20, 5.584, 14.4019);
 	}
 
 	private void check(AffineTransformation t, double x, double y, double eX, double eY) {
